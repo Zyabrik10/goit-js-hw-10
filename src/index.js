5e2b1c1606ef0ecae86db1474ff1a7ff4b35baba
Zyabrik10 <sasamazurok@gmail.com>
2023-04-24 21:49:43 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
-import './css/styles.css';
+// const DEBOUNCE_DELAY = 300;
 
-const DEBOUNCE_DELAY = 300;
+const search_box = document.querySelector('#search-box');
+const country_list = document.querySelector('.country-list');
+const country_info = document.querySelector('.country-info');
+
+search_box.addEventListener('input', _.debounce(() => {
+  if (search_box.value.trim() === '') {
+    country_list.innerHTML = '';
+    country_info.innerHTML = '';
+    return
+  };
+
+  fetch(`https://restcountries.com/v3.1/name/${search_box.value}?fields=name,capital,population,flags,languages`)
+    .then(responce => {
+      if (!responce.ok) {
+        throw new Error(responce.status);
+      }
+
+      return responce.json();
+    })
+    .then(data => {
+      const countrys = data.slice();
+
+      let list = '';
+
+      if (countrys.length === 1){
+        const country = countrys[0];
+
+        const languages = Object.values(country.languages).join(', ');
+
+        country_info.innerHTML = `
+            <ul class="conutry-info-list list">
+                <li class="conutry-info-item conutry-info-name-flag">
+                    <img src='${country.flags.svg}' alt="flag of ${country.name.official}" width="40">
+                    <h1 class="hed">${country.name.official}</h1>
+                </li>
+                <li class="conutry-info-item">
+                    <p class="par"><span class="info">Population:</span> ${country.population}</p>
+                </li>
+                <li class="conutry-info-item">
+                    <p class="par"><span class="info">Capital:</span> ${country.capital}</p>
+                </li>
+                <li class="conutry-info-item">
+                    <p class="par"><span class="info">Languages:</span> ${languages}</p>
+                </li>
+            </ul>
+        `;
+      }else if (countrys.length > 10){
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      }else{
+        list = countrys
+        .map(country => {
+            return `
+                <li class="country-item">
+                    <img src='${country.flags.svg}' alt="flag of ${country.name.official}" width="25">
+                    <p class="par">${country.name.common}</p>
+                </li>
+            `;
+        })
+        .join('\n');
+
+        country_info.innerHTML = '';
+    }
+
+    country_list.innerHTML = list;
+})
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      country_list.innerHTML = '';
+      country_info.innerHTML = '';
+    });
+}, 300));
