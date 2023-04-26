2cbe15181613b4acc70ddf7b318f03705a1cde28
Zyabrik10 <sasamazurok@gmail.com>
2023-04-26 14:17:18 +0300

    Done

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,12 @@
+export function fetchCountries(name){
+    return fetch(
+        `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+      )
+        .then(responce => {
+          if (!responce.ok) {
+            throw new Error(responce.status);
+          }
+  
+          return responce.json();
+        });
+}
